beaeebaeca277380da1b245ecdce7b51d2b87af3
Bjorn Idrizi <idrizibjorn@gmail.com>
2026-09-11 14:48:06 +0200

    Make SD static with const role name

--- CarRental.Utility/SD.cs
+++ CarRental.Utility/SD.cs
@@ -1,7 +1,7 @@
 ﻿namespace CarRental.Utility
 {
-    public class SD
+    public static class SD
     {
-        public static string AdminRole = "Admin";
+        public const string AdminRole = "Admin";
     }
 }
